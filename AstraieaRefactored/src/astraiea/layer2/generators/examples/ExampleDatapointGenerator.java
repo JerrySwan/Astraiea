7cefeadbc6b402cf84cbdbfc70d21a8af2647253
jsw <jsw@03ea2287-a2a3-45ce-82c4-f3d30a4527ca>
2016-02-17 09:03:02 +0000

    Renamed package.

---
+++ AstraieaRefactored/src/astraiea/layer2/generators/examples/ExampleDatapointGenerator.java
@@ -0,0 +1,42 @@
+package astraiea.layer2.generators.examples;
+
+import java.util.Random;
+
+import astraiea.layer2.generators.Generator;
+import astraiea.layer2.generators.PairGeneratorOutput;
+
+/**
+ * The Example generator for just vanilla (double, boolean pair valued) generator 
+ * rather than a timeseries or generator with multiple artefacts used in the JUnit tests.
+ * 
+ * @author Geoffrey Neumann
+ *
+ */
+
+public class ExampleDatapointGenerator implements Generator<PairGeneratorOutput> {
+
+	private double bias;
+	private double gap;
+
+	/**
+	 * @param bias with what probability this class should generate higher values
+	 * @param gap by how much, on average, the higher values 
+	 * generated from this class should be higher than the lower ones
+	 */
+	public ExampleDatapointGenerator(double bias, double gap){
+		this.bias = bias;
+		this.gap = gap;
+	}
+	
+	@Override
+	public PairGeneratorOutput generate(Random random) {
+		double val = random.nextGaussian();
+		if(random.nextDouble() > bias)
+			val += gap;
+		boolean bool = (val > bias);
+		return new PairGeneratorOutput(val,bool);
+	}
+}
+
+// End ///////////////////////////////////////////////////////////////
+
